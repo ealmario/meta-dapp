172b5efc465c864fdf8a6949d4352a2045fb6544
ealmario <ee.almario@gmail.com>
2020-07-31 16:42:34 +0800

    Fix 404 page

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 const Page404 = () => {
   return(
     <>
-      <div className="jumbotron jumbotron-fluid hero-section d-flex justify-content-center align-items-center m-0">
-        <div className="container d-flex justify-content-center align-items-center flex-column">
-          <h1 className="m-0">Sorry</h1>
-          <h2 className="mb-3">Page you requested can't be found</h2>
-          <Link class="btn btn-primary" to="/">Go Back to Home</Link>
-        </div>
+      <div className="container content-container d-flex justify-content-center align-items-center flex-column">
+        <h1 className="m-0">Sorry</h1>
+        <h2 className="mb-3">Page you requested can't be found</h2>
+        <Link class="btn btn-custom" to="/">Go Back to Home</Link>
       </div>
     </>
   )
